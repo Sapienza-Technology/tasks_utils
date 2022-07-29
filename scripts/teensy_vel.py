53d23d62db5594a0d13744cf580b4528a0f27546
Alessio Parmeggiani <parmeggiani.alessio1@gmail.com>
2022-07-29 15:12:37 +0200

    teensy thingies

---
+++ scripts/teensy_vel.py
@@ -0,0 +1,40 @@
+
+import rospy
+from std_msgs.msg import String, Float32
+#import twist message
+from  geometry_msgs.msg import Twist
+
+R=0.08
+L=0.515
+enc_res=356.3
+
+def callback(data,pubR,pubL,rate):
+    #print("received data: ", data)
+    linear=data.linear.x
+    angular=data.angular.z
+    V_r=linear+angular*L/2
+    V_l=linear-angular*L/2
+    W_r=V_r/R
+    W_l=V_l/R
+    print("V_r: ", W_r, "V_l: ", W_l)
+    #publish
+    pubR.publish(W_r)
+    pubL.publish(W_l)
+    rate.sleep()
+    
+
+def main():
+    pubR = rospy.Publisher('/destro', Float32, queue_size=10)
+    pubL = rospy.Publisher('/sinistro', Float32, queue_size=10)
+    rospy.init_node('firmware_teensy', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+
+    #subscriber pass data using lambda function
+    rospy.Subscriber("/cmd_vel", Twist, lambda x: callback(x,pubR,pubL,rate))
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
