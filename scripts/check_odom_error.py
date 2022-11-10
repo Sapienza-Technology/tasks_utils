4d7950a4aa6faf586b504ef52f771b02df13e987
alessio <parmeggiani.1799327@studenti.uniroma1.it>
2022-11-10 17:13:17 +0100

    fixed ground truth odom, added node to plot

--- scripts/check_odom_error.py
+++ scripts/check_odom_error.py
@@ -7,6 +7,8 @@ from math import sin
 import numpy as np
 from math import sqrt
 import message_filters
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+
 
 '''
 This node take the estimated position and the ground truth position and compute the error between them
@@ -14,27 +16,33 @@ It's important to notice that the ground truth position initially is with respec
 we need it to be with respect to the robot starting position
 '''
 
-
-def measure_error(est_odom,gt_odom,pub,rate):
-    
-    #estimated position
-    x=est_odom.pose.pose.position.x
-    y=est_odom.pose.pose.position.y
-    z=est_odom.pose.pose.position.z
-    print("estimated position|  x:%.4f  y:%.4f  z:%.4f" % (x,y,z))
-
-    #Ground truth position
-
+def convert_gt(gt_odom,start_gt_pose):
     #global coordinates
     gt_x=gt_odom.pose.pose.position.x
     gt_y=gt_odom.pose.pose.position.y
     gt_z=gt_odom.pose.pose.position.z
 
     #robot starting position
-    r_x= -1.1046
-    r_y= -14.8783
-    r_z= 1.16
-    theta=1.7
+    r_x= start_gt_pose.pose.pose.position.x
+    r_y= start_gt_pose.pose.pose.position.y
+    r_z= start_gt_pose.pose.pose.position.z
+
+
+    #covnert quaternion to euler
+    q = start_gt_pose.pose.pose.orientation
+
+    '''
+    siny_cosp = 2 * (q.w * q.z + q.x * q.y)
+    cosy_cosp = 1 - 2 * (q.y * q.y + q.z * q.z)
+    yaw = np.arctan2(siny_cosp, cosy_cosp)
+    '''
+
+    orientation_list = [q.x, q.y, q.z, q.w]
+    (roll, pitch, yaw) = euler_from_quaternion (orientation_list)
+    
+
+    theta= yaw
+    
     '''
     #homogeneous matrix 2d
     #starting pose of robot
@@ -58,46 +66,72 @@ def measure_error(est_odom,gt_odom,pub,rate):
     gt_x=local_pos[0]
     gt_y=local_pos[1]
 
-    print("Real position|  x:%.4f  y:%.4f  z:%.4f " % (gt_x,gt_y,z))
+    print("Real position|  x:%.4f  y:%.4f  z:%.4f " % (gt_x,gt_y,gt_z))
     
+    #header
     
-    #distance between ground truth position and estimated position
+    new_gt=Odometry()   
+    new_gt.header.stamp = rospy.Time.now()
+    new_gt.header.frame_id = "odom"
+    new_gt.pose.pose.position.x=gt_x
+    new_gt.pose.pose.position.y=gt_y
+    new_gt.pose.pose.position.z=gt_z
+    
+    #current_gt_pose.child_frame_id = "base_footprint"
+
+    return new_gt
+
+    
+
+
+def measure_error(est_odom,gt_odom,pub,rate,start_gt_pose):
+    
+    if not start_gt_pose: return
+    
+    #estimated position
+    x=est_odom.pose.pose.position.x
+    y=est_odom.pose.pose.position.y
+    z=est_odom.pose.pose.position.z
+    print("estimated position|  x:%.4f  y:%.4f  z:%.4f" % (x,y,z))
+
+    #Ground truth position
+    gt_odom=convert_gt(gt_odom,start_gt_pose)
+    gt_odom.pose.pose.position.z=z
+
+    # publish the message
+    pub.publish(gt_odom)
+    rate.sleep()
+
+    gt_x=gt_odom.pose.pose.position.x
+    gt_y=gt_odom.pose.pose.position.y
+    #euclidean distance between ground truth position and estimated position
     error=sqrt((gt_x-x)**2+(gt_y-y)**2)
     print("error: %.4f" % error)
     print()
-    #publish ground truth position in correct frame
-    if not rospy.is_shutdown():
-        odom = Odometry()
-        current_time = rospy.Time.now()
-        odom.header.stamp = current_time
-        odom.header.frame_id = "map"
-
-        # set the position
-        odom.pose.pose = Pose(Point(gt_x, gt_y, 0.), est_odom.pose.pose.orientation)
-
-        # set the velocity
-        odom.child_frame_id = "base_footprint"
-        #odom.twist.twist = Twist(Vector3(vx, vy, 0), Vector3(0, 0, vth))
-
-        # publish the message
-        pub.publish(odom)
-        rate.sleep()
+    
 
 
 def check_odom_error():
+    print("Starting odom error node")
     rospy.init_node('check_odom', anonymous=True)
 
+    #get only a sinlge message, used to set the global starting position of the robot in gazebo
+    start_gt_pose = rospy.wait_for_message('/ground_truth', Odometry, timeout=20)
+
+    #fixed ground truth, wth respect to starting position
     pub = rospy.Publisher('/ground_truth2', Odometry, queue_size=10)
     rate = rospy.Rate(10) # 10hz
 
     est_odom = message_filters.Subscriber('/odom', Odometry)
     gt_odom = message_filters.Subscriber('/ground_truth', Odometry)
 
-
     queue_size=10
-    ts = message_filters.TimeSynchronizer([est_odom, gt_odom], queue_size)
-    ts.registerCallback(lambda est_odom, gt_odom: measure_error(est_odom,gt_odom,pub,rate))
+    ts = message_filters.ApproximateTimeSynchronizer([est_odom, gt_odom],queue_size,0.1)
+    ts.registerCallback(lambda est_odom, gt_odom: measure_error(est_odom,gt_odom,pub,rate,start_gt_pose))
+
+
     rospy.spin()
+
 
 
 if __name__ == '__main__':
