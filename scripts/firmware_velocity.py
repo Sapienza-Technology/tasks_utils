2ef1405cae371775b07f1c0104d46f1fcf9b86bb
Jetson <parmeggiani.alessio1@gmail.com>
2023-01-03 10:25:52 +0100

    firmware velocity

--- scripts/firmware_velocity.py
+++ scripts/firmware_velocity.py
@@ -14,12 +14,25 @@ def callback(data,pubR,pubL,rate):
     #print("received data: ", data)
     linear=data.linear.x
     angular=data.angular.z
+    """ 
+    in_place_vel=None
+    try:
+        in_place_vel=rospy.get_param("/move_base/TrajectoryPlannerROS/min_in_place_vel_theta")
+    except:
+        print("parameter in place velocity not found")
+        pass
+    if angular<0.2 and in_place_vel and angular != in_place_vel:
+        linear=linear*1.5 
+        angular=angular*1.7 
+    """
     V_r=linear+angular*L/2
     V_l=linear-angular*L/2
     W_r=V_r/R
     W_l=V_l/R
-    print(f"Received velocity - l:{linear} a:{angular}\n Publishing velocity to firmware - right: {W_V_r} left: {W_l}")
+    print("received\t  angular:  ",angular,"linear:  ",linear)
+    print(f"Publishing velocity to firmware - right: {W_r} left: {W_l}")
     #publish
+    print("publishing velocities to microcontroller")
     pubR.publish(W_r)
     pubL.publish(W_l)
     rate.sleep()
@@ -28,8 +41,10 @@ def callback(data,pubR,pubL,rate):
 def main():
     pubR = rospy.Publisher('/destra', Float32, queue_size=10)
     pubL = rospy.Publisher('/sinistra', Float32, queue_size=10)
+    
     rospy.init_node('firmware_teensy', anonymous=True)
-    rate = rospy.Rate(100)
+    rate = rospy.Rate(10)
+    print("\n\n\navviato nodo firmware velocity\n\n\n")
 
     #subscriber pass data using lambda function
     rospy.Subscriber("/cmd_vel", Twist, lambda x: callback(x,pubR,pubL,rate))
