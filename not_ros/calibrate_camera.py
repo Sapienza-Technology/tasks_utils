126ca8baa65e8b3b3745578344ff4f5c17989114
Alessio Parmeggiani <parmeggiani.alessio1@gmail.com>
2023-08-28 21:50:27 +0200

    updates on firmware and camera calibration

--- not_ros/calibrate_camera.py
+++ not_ros/calibrate_camera.py
@@ -3,6 +3,9 @@ import cv2
 
 import os
 import json
+import yaml
+
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 # Number of object points 
 num_intersections_in_x = 10  
 num_intersections_in_y = 7
@@ -13,6 +16,60 @@ square_size = 0.025
 # Arrays to store 3D points and 2D image points
 obj_points = []
 img_points = []
+
+
+def create_param_file(params):
+    #create a yaml file in the format:
+    '''
+    image_width: 640
+    image_height: 480
+    camera_name: usb_cam
+    camera_matrix:
+    rows: 3
+    cols: 3
+    data: [445.10792629,   0.0 ,337.72928031,  0.0 ,  445.48201089 ,246.31395522,  0.0,0.0,1.0]
+    distortion_model: plumb_bob
+    distortion_coefficients:
+    rows: 1
+    cols: 5
+    data: [ 0.11847053, -0.35759139 , 0.00148395,  0.00276497 , 0.61140408]
+    rectification_matrix:
+    rows: 3
+    cols: 3
+    data: [1, 0.0, 0.0, 0.0, 1, 0.0, 0.0, 0.0, 1]
+    projection_matrix:
+    rows: 3
+    cols: 4
+    data: [445.10792629,   0.0 ,        337.72928031, 0.0, 445.48201089 ,246.31395522, 0.0, 0.0, 1.0, 0.000000, 1.000000, 0.000000]
+    '''
+
+    #create a dictionary
+    data = dict()
+    data['image_width'] = params['width']
+    data['image_height'] = params['height']
+    data['camera_name'] = 'usb_cam'
+    data['camera_matrix'] = dict()
+    data['camera_matrix']['rows'] = 3
+    data['camera_matrix']['cols'] = 3
+    data['camera_matrix']['data'] = params['K'].flatten().tolist()
+    data['distortion_model'] = 'plumb_bob'
+    data['distortion_coefficients'] = dict()
+    data['distortion_coefficients']['rows'] = 1
+    data['distortion_coefficients']['cols'] = 5
+    data['distortion_coefficients']['data'] = params['D'].flatten().tolist()
+    data['rectification_matrix'] = dict()
+    data['rectification_matrix']['rows'] = 3
+    data['rectification_matrix']['cols'] = 3
+    data['rectification_matrix']['data'] = [1, 0.0, 0.0, 0.0, 1, 0.0, 0.0, 0.0, 1]
+    data['projection_matrix'] = dict()
+    data['projection_matrix']['rows'] = 3
+    data['projection_matrix']['cols'] = 4
+    data['projection_matrix']['data'] = params['K'].flatten().tolist() + [0.0, 1.0, 0.0, 0.0]
+
+    #write in a yaml file
+    with open ('params/camera_params.yaml', 'w') as f:
+        #prettu print
+        yaml.dump(data, f, default_flow_style=None)
 
 # Prepare expected object 3D object points (0,0,0), (1,0,0) ...
 object_points = np.zeros((num_intersections_in_x*num_intersections_in_y,3), np.float32)
@@ -36,7 +93,9 @@ for img_name in imgs:
     # If found, add object points, image points (after refining them)
     if ret == True:
         obj_points.append(object_points)
-        img_points.append(corners)
+        #refine
+        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+        img_points.append(corners2)
 
         # Draw and display the corners
         drawn_img = cv2.drawChessboardCorners(img, (num_intersections_in_x,num_intersections_in_y), corners,ret)
@@ -55,7 +114,6 @@ D=np.array(dist)
 print("K = ", K)
 print("D = ", D)
 
-#write in a json file
-with open ('not_ros/camera_params.json', 'w') as f:
-    #prettu print
-    json.dump({'K': K.tolist(), 'D': D.tolist()}, f, indent=4, sort_keys=True)
+params={'K': K, 'D': D, "width": img_shape[0], "height": img_shape[1]}
+
+create_param_file(params)
--- scripts/firmware_CC8.py
+++ scripts/firmware_CC8.py
@@ -6,21 +6,30 @@ from  geometry_msgs.msg import Twist
 #import float32 multiarray
 from std_msgs.msg import Float32MultiArray
 import math
-
+import time
+import numpy as np
 wheel_radius=0.8
-d=0.6 #distance between the wheels along y axis
+d=0.94 #distance between the wheels along y axis
 
 #FRONT, MID, REAR (first left, then right)
 wheel_positions=[ 
-    [0.3,0.3],
-    [0.3,-0.3],
-    [0,0.3],
-    [0,-0.3],
-    [-0.3,0.3],
-    [-0.3,-0.3]
+    [0.245,0.4],
+    [0.245,-0.4],
+    [0,0.47],
+    [0,-0.47],
+    [-0.2575,0.43],
+    [-0.2575,-0.43]
 ]
 
-max_steer=math.pi/2
+max_steer=math.pi/4
+MAX_V=1
+MAX_W=1/wheel_radius
+IN_PLACE_VEL= MAX_V/2
+
+def linear2angular(l):
+    new_l=l/0.08
+    return new_l.tolist()
+
 '''
 Given a twist message, compute the wheel velocities
 for a 6 driving wheels rover with 4 steering wheels
@@ -33,6 +42,7 @@ def compute_wheel_velocities(data):
 
     v=data.linear.x
     w=data.angular.z
+    #w=-w #TODO resolve?
 
     turning_radius=None
     turning_rate=None
@@ -44,7 +54,7 @@ def compute_wheel_velocities(data):
     #IN PLACE ROTATION
     if abs(w)>0.001 and abs(v)<0.001:
         print("in place rotation")
-        wheel_angles=[max_steer,-max_steer,max_steer,-max_steer]
+        wheel_angles=[-max_steer,max_steer,max_steer,-max_steer]
         
         for i in range(len(wheel_positions)):
             wheel_x=wheel_positions[i][0]
@@ -52,10 +62,13 @@ def compute_wheel_velocities(data):
             
             r=math.sqrt(wheel_x*wheel_x+wheel_y*wheel_y)
 
-            #camba segno se ruota in senso sbagliato
-            sgn= 1 if wheel_y > 0 else -1
+            #cambia segno se ruota in senso sbagliato
+            sgn= 1 if wheel_y < 0 else -1
+            if w<0: 
+                sgn*=-1
             vel = w*r*sgn
-            wheel_velocities[i]=vel/wheel_radius
+            wheel_velocities[i]=IN_PLACE_VEL*sgn
+        wheel_velocities=linear2angular(np.array(wheel_velocities))
         return wheel_velocities,wheel_angles
         
 
@@ -92,6 +105,7 @@ def compute_wheel_velocities(data):
             wheel_y=wheel_positions[i][1]
 
             angle=math.atan2(wheel_x,turning_radius-wheel_y)
+            #angle=math.pi/2-angle
 
             #flip angle by 180 deg if steering rotation is past 90 deg
             if angle > math.pi/2:
@@ -99,19 +113,39 @@ def compute_wheel_velocities(data):
             elif angle < -math.pi/2:
                 angle+=math.pi
             
-            if angle > max_steer:
-                print("WARN: angle > max_steer, angle: ",angle)
-            wheel_angles[steer_idx]=min(angle,max_steer)
+            #if angle > max_steer:
+            #    print("WARN: angle > max_steer, angle: ",angle)
+            #wheel_angles[steer_idx]=min(angle,max_steer)
+            wheel_angles[steer_idx]=angle
             steer_idx+=1
-    
+    wheel_velocities=linear2angular(np.array(wheel_velocities))
     return wheel_velocities,wheel_angles
 
-def callback(data,pub,rate):
+def callback(data,pub,rate,params):
     velocities,angles=compute_wheel_velocities(data)
     print("input linear: "+str(data.linear.x)+" angular: "+str(data.angular.z))
     print("output velocities: "+str(velocities)+" angles: "+str(angles))
-    #pub.publish(velocities)
-    #rate.sleep()
+
+    msg=Float32MultiArray()
+    if abs(angles[0])==max_steer and abs(angles[1])==max_steer:
+        if params["in_place_configuration"]==False:
+            params["in_place_configuration"]=True
+            print("IN place, sending only steer")
+            temp_velocities=[0,0,0,0,0,0]
+            msg.data=temp_velocities+angles
+            pub.publish(msg)
+            time.sleep(3)
+            print("IN place, starting rotation")
+    else:
+        params["in_place_configuration"]=False
+
+
+    #create multliarray
+    msg.data=velocities+angles
+
+    pub.publish(msg)
+
+    rate.sleep()
     
 
 def main():
@@ -120,9 +154,12 @@ def main():
     rospy.init_node('firmware_teensy', anonymous=True)
     rate = rospy.Rate(10)
     print("\n\n\navviato nodo firmware velocity\n\n\n")
-
+    params={
+        "in_place_configuration": False,
+        "current_configuration": "linear", #linear or in place
+    }
     #subscriber pass data using lambda function
-    rospy.Subscriber("/cmd_vel", Twist, lambda x: callback(x,pub,rate))
+    rospy.Subscriber("/cmd_vel", Twist, lambda x: callback(x,pub,rate,params))
     rospy.spin()
 
 if __name__ == '__main__':
